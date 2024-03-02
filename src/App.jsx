f1d31228e01f29f9010495976d6106b016023229
jaykumar286 <jaykumarchavda2806@gmail.com>
2024-03-02 22:17:47 +0530

    add an toggle to change theme and also persist that theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import './App.css'
-import MainRoutes from './routes/MainRoutes'
-import Navbar from './components/Navbar/Navbar'
+import "./App.css";
+import MainRoutes from "./routes/MainRoutes";
+import Navbar from "./components/Navbar/Navbar";
+import ThemeContext from "./context/ThemeContext";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(()=>{
+    const userTheme = localStorage.getItem('app-theme');
+    if (userTheme!=null){
+      setTheme(userTheme);
+    }
+  }
+  ,[])
+
   return (
-    <div id='app-wrapper'>
-    <Navbar/>
-    <MainRoutes/>
-    </div>
-  )
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div id="app-wrapper" data-theme={theme}>
+        <Navbar />
+        <MainRoutes />
+      </div>
+    </ThemeContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import { useState,useContext } from "react";
 import useMovieList from "../../hooks/useMovieList";
 import useDebounce from "../../hooks/useDebounce";
 import "./Navbar.css";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon,faSun } from '@fortawesome/free-solid-svg-icons';
+import { Link } from "react-router-dom";
+import ThemeContext from "../../context/ThemeContext";
 
 export default function Navbar() {
   const [isListVisibile,setIsListVisible] = useState(false);
@@ -10,13 +14,26 @@ export default function Navbar() {
   const { movieList } = useMovieList(searchTerm ? searchTerm : 'avengers');
   const navigator = useNavigate();
 
+  const {theme,setTheme} = useContext(ThemeContext);
+
   function handleClick(e,id){
     navigator(`/movie/${id}`);  
   }
 
+  function updateTheme(){
+    if (theme =='dark'){
+      setTheme('light');
+      localStorage.setItem('app-theme','light');
+    }
+    else{
+      setTheme('dark');
+      localStorage.setItem('app-theme','dark');
+    }
+  }
+
   return (
     <div className="navbar-wrapper">
-      <div className="logo">MovieHub</div>
+      <div className="logo"><Link to="/">MovieHub</Link></div>
       <div className="search">
         <input
           type="text"
@@ -34,7 +51,9 @@ export default function Navbar() {
         </div>
       </div>
 
-      <div className="theme">Theme</div>
+      <div className="theme-icon" onClick={updateTheme}>
+        <FontAwesomeIcon icon={theme=='dark'?faSun:faMoon}/>
+      </div>
     </div>
   );
 }
